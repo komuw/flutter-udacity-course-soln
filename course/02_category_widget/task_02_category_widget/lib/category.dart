0e5ffa588538f60f8d5bf0d8bee440981397f0e2
komu <komuw05@gmail.com>
2018-05-15 18:01:51 +0300

    add category widget

--- course/02_category_widget/task_02_category_widget/lib/category.dart
+++ course/02_category_widget/task_02_category_widget/lib/category.dart
@@ -16,7 +16,11 @@ class Category extends StatelessWidget {
   /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
   /// the UI, and the icon that represents it (e.g. a ruler).
   // TODO: You'll need the name, color, and iconLocation from main.dart
-  const Category();
+  final String _categoryName;
+  final MaterialColor _categoryColor;
+  final IconData _categoryIcon;
+
+  const Category(this._categoryName, this._categoryColor, this._categoryIcon);
 
   /// Builds a custom widget that shows [Category] information.
   ///
@@ -28,6 +32,33 @@ class Category extends StatelessWidget {
   // See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
     // TODO: Build the custom widget here, referring to the Specs.
-    return Container();
+    final Icon _icon = Icon(_categoryIcon);
+    final Text _txt2 = Text(_categoryName, textAlign: TextAlign.right);
+
+    final Padding _padding = Padding(
+      child: _icon,
+      padding: EdgeInsets.only(bottom: 0.0, top: 0.0, left: 80.0, right: 80.0),
+    );
+    final Center _center = Center(
+      child: _txt2,
+      widthFactor: 2.0,
+    );
+
+    final Row _row = Row(
+      children: <Widget>[_padding, _center],
+    );
+    final InkWell _inkwell = InkWell(
+      borderRadius: BorderRadius.circular(50.0),
+      child: _row,
+    );
+    final Container _cont = Container(
+      height: 100.00,
+      padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+      child: _inkwell,
+    );
+
+    return Material(
+      child: _cont,
+    );
   }
 }
